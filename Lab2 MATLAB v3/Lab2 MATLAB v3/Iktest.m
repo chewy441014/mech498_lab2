f0d494be269a5435a094117cf25f26dab1f64d35
chewy441014 <p.hill.ultimate@gmail.com>
2016-03-08 23:34:47 -0600

    initial commit
    delicious

---
+++ Lab2 MATLAB v3/Lab2 MATLAB v3/Iktest.m
@@ -0,0 +1,13 @@
+%IK_test
+
+prev_joint_angles=[0,0,0,0,0,0];
+
+fanuc=fanucInit;
+
+joint_angles=[pi/4,pi/4,pi/4,0,0,0];
+
+[~,T,~] = fanucFK(joint_angles,fanuc);
+
+[is_solution,joint_angles_new]=fanucIK(T,prev_joint_angles,fanuc)
+
+drawFanuc(joint_angles_new,fanuc)
---
+++ Lab2 MATLAB v3/Lab2 MATLAB v3/dhtf.m
@@ -0,0 +1,16 @@
+%Ian Tomkinson
+%Robotics Lab 2, Q1
+%angles are assumed to be in radians
+
+function T = dhtf(alpha,a,d,theta)
+
+T= [cos(theta), -sin(theta), 0, a; 
+    sin(theta)*cos(alpha), cos(theta)*cos(alpha), ...
+    -sin(alpha), -sin(alpha)*d; 
+    sin(theta)*sin(alpha), cos(theta)*sin(alpha), ...
+    cos(alpha), cos(alpha)*d; 
+    0, 0, 0, 1];
+
+
+
+end
